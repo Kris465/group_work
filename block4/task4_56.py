7ebadd770a64b3187b3b6729b000f2f30fedb687
unknown <valeriya9169566@gmaie.com>
2026-05-18 20:21:15 +0300

    task4_62 is done

---
+++ block4/task4_56.py
@@ -0,0 +1,14 @@
+ab =int(input("Введите двухзначное число ab:"))
+if a == 4 or b == 7:
+    print("числа входят")
+if a == 7 or b == 4:
+     print("числа входят")
+elif a == 3 or b == 6:
+    print("числа входят")
+elif a == 6 or b == 3:
+    print("числа входят")
+elif a == 9 or b == 9:   
+    print("числа входят") 
+else:
+     print("числа невходят")
+    
